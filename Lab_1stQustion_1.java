fdcf12e5d822cf9e249159260f3e2ab072ec0caf
KUMARIKAJOL <122786014+KUMARIKAJOL@users.noreply.github.com>
2023-02-02 18:04:30 +0530

    Add files via upload

---
+++ Lab_1stQustion_1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Lab_1stQustion_1 {
+	 public static void main(String[] args) {
+		  Scanner in = new Scanner(System.in);
+		   
+		  System.out.print("Input first number: ");
+		  int num1 =10; in.nextInt();
+		   
+		  System.out.print("Input second number: ");
+		  int num2 =20; in.nextInt();
+		   
+		  System.out.print("Input third number: ");
+		  int num3 = 30; in.nextInt();
+		   
+		  System.out.print("Input fourth number: ");
+		  int num4 =40; in.nextInt();
+		  
+		  System.out.print("Enter fifth number: ");
+		  int num5 = 50;in.nextInt();
+		   
+		   
+		  System.out.println("Average of five numbers is: " + 
+		   (num1 + num2 + num3 + num4 + num5) / 5);
+		 }
+		}
+
+		
+
+
